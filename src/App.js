f402e731dd14eb72ec5832e73284ad4f788d3961
akmaluddinf <akmaluddinfadhilah@gmail.com>
2021-09-17 14:31:53 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home'
+import IndexPost from './pages/posts/Index';
+import CreatePost from './pages/posts/Create';
+import EditPost from './pages/posts/Edit';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand to="/">EXPRESS.JS + REACT.JS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/" className="nav-link">HOME</Nav.Link>
+                  <Nav.Link as={Link} to="/posts" className="nav-link">POSTS</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={IndexPost} />
+        <Route exact path="/posts/create" component={CreatePost} />
+        <Route exact path="/posts/edit/:id" component={EditPost} />
+      </Switch>
+      
     </div>
   );
 }
